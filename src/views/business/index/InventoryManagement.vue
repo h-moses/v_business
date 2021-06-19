d04c33095388ad48c1999d838461ada25d9d1a7a
h_admin <709820314@qq.com>
2021-06-19 21:55:39 +0800

    库存管理

--- src/views/business/index/InventoryManagement.vue
+++ src/views/business/index/InventoryManagement.vue
@@ -35,12 +35,62 @@
                                     <v-col>
                                         <span>库存：15件</span>
                                     </v-col>
-                                    <v-col>
-                                        <span>单价：15.00/公斤</span>
-                                    </v-col>
                                 </v-row>
                             </v-list-item-subtitle>
                         </v-list-item-content>
+                        <v-list-item-action>
+                            <v-menu top offset-y transition="scale-transition">
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn v-bind="attrs" v-on="on" icon>
+                                        <v-icon>mdi-dots-horizontal</v-icon>
+                                    </v-btn>
+                                </template>
+                                <v-row class="op-row" align="center" justify="center">
+                                    <v-dialog v-model="edit_dialog" persistent max-width="500" transition="dialog-bottom-transition">
+                                        <template v-slot:activator="{on,attrs}">
+                                            <v-btn v-bind="attrs" v-on="on" text>编辑</v-btn>
+                                        </template>
+                                        <v-card>
+                                            <v-card-title>
+                                                <span class="text-h6">编辑库存</span>
+                                            </v-card-title>
+                                            <v-card-text>
+                                                <v-container>
+                                                    <v-row>
+                                                        <v-col cols="12" sm="12">
+                                                            <v-text-field label="商品名称" :rules="[editRules.required]"></v-text-field>
+                                                        </v-col>
+                                                        <v-col cols="12" sm="12">
+                                                            <v-text-field label="库存数量" :rules="[editRules.required]"></v-text-field>
+                                                        </v-col>
+                                                    </v-row>
+                                                </v-container>
+                                                <v-card-actions>
+                                                    <v-spacer/>
+                                                    <v-btn text @click="edit_dialog = false">取消</v-btn>
+                                                    <v-btn text @click="editStore">确认</v-btn>
+                                                </v-card-actions>
+                                            </v-card-text>
+                                        </v-card>
+                                    </v-dialog>
+                                    <v-divider vertical/>
+                                    <v-dialog v-model="delete_dialog" persistent max-width="290" transition="dialog-bottom-transition">
+                                        <template v-slot:activator="{on,attrs}">
+                                            <v-btn v-bind="attrs" v-on="on" text>删除</v-btn>
+                                        </template>
+                                        <v-card>
+                                            <v-card-title>删除库存</v-card-title>
+                                            <v-card-text class="dialog-card">确认删除该库存?</v-card-text>
+                                            <v-card-actions>
+                                                <v-spacer/>
+                                                <v-btn text @click="delete_dialog = false">取消</v-btn>
+                                                <v-btn text @click="deleteStore">确认</v-btn>
+                                            </v-card-actions>
+                                        </v-card>
+                                    </v-dialog>
+                                </v-row>
+                            </v-menu>
+                        </v-list-item-action>
                     </v-list-item>
                 </v-list>
             </v-container>
@@ -55,12 +105,23 @@
             return {
                 card_title: [
                     '商品总数','负库存数','库存预警'
-                ]
+                ],
+                delete_dialog: false,
+                edit_dialog: false,
+                editRules: {
+                    required: value => !!value || '不能为空'
+                }
             }
         },
         methods: {
             backRoute() {
                 this.$router.back()
+            },
+            deleteStore() {
+
+            },
+            editStore() {
+
             }
         }
     }
@@ -90,6 +151,10 @@
 
     .v-list-item__content {
         padding: 12px 0 0;
+    }
+
+    .dialog-card {
+        text-align: start;
     }
 
 </style>
